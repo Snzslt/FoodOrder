68d77c25de829e3ff9e6f0be4a3e05559500b46e
Sanaz Soltani <74157401+Snzslt@users.noreply.github.com>
2024-01-30 15:41:44 -0500

    Using the CartContext & Reducer/ Adding Reusavle Model Componwnt with useEffet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./Components/Header";
 import Meals from "./Components/Meals";
+import {CartContextProvider} from "./store/CartContext";
 function App() {
   return (
     <>
+    <CartContextProvider>
      <Header />
      <Meals />
+    </CartContextProvider>
+    
     </>
   );
 }
--- src/Components/MealsItem.jsx
+++ src/Components/MealsItem.jsx
@@ -1,6 +1,15 @@
+import { useContext } from "react";
 import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
+import CartContext from "../store/CartContext.jsx";
+
+
 export default function MealItem({meal}){
+    const cartCtx = useContext(CartContext);
+
+    function handleAddMealToCart(){
+        cartCtx.addItem(meal);
+    }
 
     return (
         <li className="meal-item">
@@ -12,7 +21,7 @@ export default function MealItem({meal}){
             <p className="meail-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-            <Button> Add to Cart</Button>
+            <Button onClick = {handleAddMealToCart}> Add to Cart</Button>
 
         </p>
 
